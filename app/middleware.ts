4e8e3533dd093e0e79a969d6ee3fa7409e635026
itecfinder <businessadviserpro@gmail.com>
2026-06-24 00:41:55 -0400

    Update middleware.ts

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,1 +1,19 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("session")?.value
+
+  const isLogin = req.nextUrl.pathname.startsWith("/login")
+  const isAuthAPI = req.nextUrl.pathname.startsWith("/api")
+
+  if (isLogin || isAuthAPI) {
+    return NextResponse.next()
+  }
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url))
+  }
+
+  return NextResponse.next()
+}
